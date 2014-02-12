6ba9e823a95bec4ce6cc7b3f29788b2c8cef0450
unknown <kaptoye@gmail.com>
2014-02-11 21:20:01 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KingVideo::Application.config.secret_key_base = '12556d49e4d74536fd8634e815a63671a319f5c51e20cbd990f9addf7d81ecf73d9e30e5ee3a0150fffd6eaa1326036beaa0285bcd20a081915cbcbe7c093714'
